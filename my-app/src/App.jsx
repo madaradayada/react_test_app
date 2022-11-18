e9038e6ab65935a85ad7aeb83e1986ef84c5694a
Oleksandr Mytrofanov <mitrofanov.322@gmail.com>
2022-11-18 18:06:50 +0200

    add_buttons_and_inputs

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,16 +1,27 @@
-import React, { useState } from 'react';
-import ClassCounter from './componets/ClassCounter';
-import Counter from './componets/Counter';
+import React, { useState } from "react";
+// import ClassCounter from "./componets/ClassCounter";
+// import Counter from "./componets/Counter";
+import "./styles/App.css";
+// import PostItem from "./componets/Postitems";
+import Postlist from "./componets/Postlist";
+import MyButton from "./componets/UI/button/MyButton";
+import MyInput from "./componets/UI/input/MyInput";
 
 function App() {
-  const [value, setValue] = useState('Текст в инпуте');
-   
+  const [posts, setPosts] = useState([
+    { id: 1, title: "Javascript", body: "Description" },
+    { id: 2, title: "Javascript 2", body: "Description" },
+    { id: 3, title: "Javascript 3", body: "Description" },
+  ]);
 
   return (
     <div className="App">
-      <ClassCounter/>
-      <ClassCounter/>
-      <ClassCounter/>
+      <form>
+        <MyInput type="text" placeholder="Название поста" />
+        <MyInput type="text" placeholder="Описание поста" />
+        <MyButton disabled>Создать пост</MyButton>
+      </form>
+      <Postlist posts={posts} title='Посты про JS'/>
     </div>
   );
 }
